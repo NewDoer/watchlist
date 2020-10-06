7897e1c03d38a9b54c8f410d7ada1e43b4ce03bc
CrazyLMX <lion@crazymcn.com>
2020-10-06 23:17:42 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+name = 'CrazyLMX'
+movies = [
+    {'title':'龙猫','year':'1988'},
+    {'title':'霸王别姬','year':'1982'},
+    {'title':'天下无贼','year':'1988'},
+    {'title':'阿凡达','year':'2008'},
+]
+
+
 @app.route('/')
 def index():
-    return "hello world "
-    
+    return render_template('index.html',name = name ,movies = movies)
+
 if __name__ == "__main__":
     app.run()
