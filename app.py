53b0691bffa54eea06c39846e84a4b5377f30bda
CrazyLMX <lion@crazymcn.com>
2020-10-07 23:09:14 +0800

    Create,edit and delete item by form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from os import name, truncate
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,flash,request,url_for
 from flask_sqlalchemy import SQLAlchemy
 import click
 
@@ -19,6 +19,7 @@ if WIN:
 else:
     prefix = 'sqlite:////'
 app = Flask(__name__)
+app.secret_key = "aaaxxx"
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -38,11 +39,54 @@ class Movie(db.Model):
     year = db.Column(db.String(4))
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        
+        if not title or not year or len(year)>4 or len(title) > 60:
+            flash("Invalid input")
+            return redirect(url_for('index'))
+        
+        movie = Movie(title=title,year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item creatd.')
+        return redirect(url_for('index'))
+    
     # user = User.query.first()
     movies = Movie.query.all()
     return render_template('index.html',movies = movies)
+
+@app.route('/movie/edit/<int:movie_id>',methods = ['GET','POST'])
+
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == "POST":
+        title = request.form.get('title')
+        year = request.form.get('year')
+
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('Invalid input')
+            return(redirect(url_for('edit',movie_id = movie_id)))
+
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash("Item updated.")
+        return redirect(url_for('index'))
+    
+    return render_template('edit.html',movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>',methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted')
+    return redirect(url_for('index'))
 
 @app.cli.command()
 def forge():
@@ -67,7 +111,7 @@ def forge():
 @app.errorhandler(404)
 def page_not_found(e):
     # user = User.query.first()
-    return render_template('404.html', user  = user), 404
+    return render_template('404.html'), 404
 
 @app.context_processor
 def inject_user():
